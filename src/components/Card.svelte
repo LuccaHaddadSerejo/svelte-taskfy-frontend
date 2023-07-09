3b2d5da980415e07dcbda462c3a35b962b7867f8
LuccaHaddadSerejo <hslucca@gmail.com>
2023-07-09 00:28:33 -0300

    feat: created SubtaskComponent

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -3,6 +3,7 @@
     import arrowup  from "../assets/arrowup.svg" 
     import trash  from "../assets/trash.svg" 
     import editTask  from "../assets/edit.svg" 
+    import SubtaskCard from "./SubtaskCard.svelte";
     let direction: boolean = false;
     $: arrow = direction ? arrowup : arrowdown 
 
@@ -11,82 +12,91 @@
     export let title:string;
 </script>
 
-<li>  
-    <div class="tasktitle">  
-        <input type="checkbox">  
-        <h2>{title}</h2>
-    </div>
-    <div class="taskbuttons">
-        <div class="taskbuttonsone">
-            <button><img src={editTask} alt="Imagem de um lixo"></button>
-            <button><img class="trash" src={trash} alt="Imagem de uma caneta e uma prancheta"></button>       
+<li> 
+    <div class="contentdiv">
+        <div class="tasktitle">  
+            <input type="checkbox">  
+            <h2>{title}</h2>
         </div>
-        <button on:click={toogleDirection}>
-            <img src={arrow} alt="Imagem de uma seta">
-        </button>
-    </div> 
+        <div class="taskbuttons">
+            <div class="taskbuttonsone">
+                <button><img src={editTask} alt="Imagem de um lixo"></button>
+                <button><img class="trash" src={trash} alt="Imagem de uma caneta e uma prancheta"></button>       
+            </div>
+            <button on:click={toogleDirection}>
+                <img src={arrow} alt="Imagem de uma seta">
+            </button>
+    </div>
+    </div>
+    {#if arrow === arrowup}
+        <ul>
+            <SubtaskCard/>
+            <SubtaskCard/>
+            <SubtaskCard/>
+        </ul>
+    {/if}
 </li>
 <style lang="scss">
-li{
-        display: flex;
-        flex-direction: column;
-        gap: 15px;
-        justify-content: space-between;
-        background-color: #262626;
-        border-radius: 8px;
-        padding: 2rem 1rem 2rem 1rem;
-
-        @media(min-width: 800px){
-            flex-direction: row;
+    li{
+            display: flex;
+            flex-direction: column;
+            gap: 15px;
+            justify-content: space-between;
+            background-color: #262626;
+            border-radius: 8px;
+            padding: 2rem 1rem 2rem 1rem;
         }
-    }
 
-    img{
-        width: 23px;
-        background-color: transparent;
-        filter: brightness(1.2);
-    }
-
-    button{
-        background-color: transparent;
-        :hover{
-            filter: brightness(1.5);
+        img{
+            width: 23px;
+            background-color: transparent;
+            filter: brightness(1.2);
         }
-    }
 
-    h2{
-        color: #cdcdcd;
-    }
+        button{
+            background-color: transparent;
+            :hover{
+                filter: brightness(1.5);
+            }
+        }
 
-    .tasktitle{
-        display: flex;
-        gap: 20px;
-    }
+        h2{
+            color: #cdcdcd;
+            font-size: clamp(1.5rem, 2vw, 2.5rem);
+        }
 
-    .taskbuttons{
-        display: flex;
-        justify-content: space-between;
-        gap: 30px;
-    }
+        .tasktitle{
+            display: flex;
+            gap: 20px;
+        }
 
-    .taskbuttonsone{
-        display: flex;
-        gap: 10px;
-    }
+        .taskbuttons{
+            display: flex;
+            justify-content: space-between;
+            gap: 30px;
+        }
 
-    .trash{
-        width: 30px;
-        position: relative;
-        top: 2px;
-    }
+        .taskbuttonsone{
+            display: flex;
+            gap: 10px;
+        }
+
+        .contentdiv{
+            display: flex;
+            flex-direction: column;
+            gap: 15px;
+            justify-content: space-between;
+
+            @media(min-width: 800px){
+                flex-direction: row;
+            }
+        }
+
+        .trash{
+            width: 30px;
+            position: relative;
+            top: 2px;
+        }
 </style>
 
 
-<!-- <ul>
-    <li>
-        <input type="checkbox">
-        <h3>Título Subtask</h3>
-        <button>Editar</button>
-        <button>Excluir</button>
-    </li>
-</ul> -->
